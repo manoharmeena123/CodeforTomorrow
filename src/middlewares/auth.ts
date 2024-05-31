4979559b6e3518e58fc049244c6137ca375370e0
manoharmeena123 <manoharmeena245@gmail.com>
2024-05-31 17:35:40 +0530

    readme added

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from '../utils/jwt';
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.token;
+  const token = req?.cookies?.token;
   if (!token) {
     return res.status(401).send('Unauthorized');
   }
